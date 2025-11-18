a3602c41fa2528dd0e4da334c582c3ba993a3b9c
CHaron1244 <118404810+CHaron1244@users.noreply.github.com>
2025-11-18 09:13:19 +0800

    2025.11.18 9.13 AM

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,19 +1,20 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,6 +36,7 @@ public class EmployeeController {
      * 登录
      *
      * @param employeeLoginDTO
+     *
      * @return
      */
     @PostMapping("/login")
@@ -43,20 +45,13 @@ public class EmployeeController {
 
         Employee employee = employeeService.login(employeeLoginDTO);
 
-        //登录成功后，生成jwt令牌
+        // 登录成功后，生成jwt令牌
         Map<String, Object> claims = new HashMap<>();
         claims.put(JwtClaimsConstant.EMP_ID, employee.getId());
-        String token = JwtUtil.createJWT(
-                jwtProperties.getAdminSecretKey(),
-                jwtProperties.getAdminTtl(),
-                claims);
+        String token = JwtUtil.createJWT(jwtProperties.getAdminSecretKey(), jwtProperties.getAdminTtl(), claims);
 
-        EmployeeLoginVO employeeLoginVO = EmployeeLoginVO.builder()
-                .id(employee.getId())
-                .userName(employee.getUsername())
-                .name(employee.getName())
-                .token(token)
-                .build();
+        EmployeeLoginVO employeeLoginVO =
+                EmployeeLoginVO.builder().id(employee.getId()).userName(employee.getUsername()).name(employee.getName()).token(token).build();
 
         return Result.success(employeeLoginVO);
     }
@@ -71,4 +66,37 @@ public class EmployeeController {
         return Result.success();
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    @ApiOperation("新增员工")
+    public Result<String> save(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("新增员工：{}", employeeDTO);
+        employeeService.save(employeeDTO);
+        return Result.success();
+    }
+
+    @RequestMapping(value = "/page", method = RequestMethod.GET)
+    @ApiOperation("分页查询员工")
+    public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO) {
+        return Result.success(employeeService.pageQuery(employeePageQueryDTO));
+    }
+
+    @RequestMapping(value = "/status/{status}", method = RequestMethod.POST)
+    @ApiOperation("启用/禁用员工状态")
+    public Result<String> startOrStop(@PathVariable("status") Integer status, @RequestParam("id") Long id) {
+        employeeService.startOrStop(status, id);
+        return Result.success();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ApiOperation("根据id查询员工")
+    public Result<Employee> getById(@PathVariable("id") Long id) {
+        return Result.success(employeeService.getById(id));
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    @ApiOperation("更新员工信息")
+    public Result<String> update(@RequestBody EmployeeDTO employeeDTO) {
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
 }
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,18 +1,46 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface EmployeeMapper {
 
-    /**
-     * 根据用户名查询员工
-     * @param username
-     * @return
-     */
+
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    @Insert("insert into employee (username,password,name,phone,sex,id_number,status,create_time,update_time,create_user,update_user)" +
+            " values(#{username},#{password},#{name},#{phone},#{sex},#{idNumber},#{status},#{createTime}," +
+            "#{updateTime},#{createUser},#{updateUser})")
+    void insert(Employee employee);
+
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
+
+    @Update({
+            "<script>",
+            "update employee",
+            "<set>",
+            "  <if test='name != null'>name = #{name},</if>",
+            "  <if test='username != null'>username = #{username},</if>",
+            "  <if test='password != null'>password = #{password},</if>",
+            "  <if test='phone != null'>phone = #{phone},</if>",
+            "  <if test='sex != null'>sex = #{sex},</if>",
+            "  <if test='idNumber != null'>id_number = #{idNumber},</if>",
+            "  <if test='updateTime != null'>update_time = #{updateTime},</if>",
+            "  <if test='updateUser != null'>update_user = #{updateUser},</if>",
+            "  <if test='status != null'>status = #{status},</if>",
+            "</set>",
+            "where id = #{id}",
+            "</script>"
+    })
+    void update(Employee employee);
+
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
